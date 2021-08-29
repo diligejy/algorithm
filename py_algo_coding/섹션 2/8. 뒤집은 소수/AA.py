65392ca62f26b743988f124ca966900ddaeb22d4
diligejy <sjy049@gmail.com>
2021-08-29 20:19:54 +0900

    뒤집은 소수

---
+++ py_algo_coding/섹션 2/8. 뒤집은 소수/AA.py
@@ -0,0 +1,24 @@
+def reverse(x):
+    res = 0
+    while x > 0:
+        t = x % 10
+        res = res * 10 + t
+        x = x // 10
+    return res
+
+
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+
+n = input()
+num_list = list(map(int, input().split()))
+
+for num in num_list:
+    if isPrime(reverse(num)) == True:
+        print(reverse(num), end=" ")
