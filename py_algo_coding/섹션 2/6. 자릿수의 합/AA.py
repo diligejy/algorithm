9388b8ab7beee5183906ebef822e086946498715
diligejy <sjy049@gmail.com>
2021-08-29 19:41:11 +0900

    에라토스테네스의 체

--- py_algo_coding/섹션 2/6. 자릿수의 합/AA.py
+++ py_algo_coding/섹션 2/6. 자릿수의 합/AA.py
@@ -1,10 +1,3 @@
-def digit_sum(x):
-    res = 0
-    for i in x:
-        res += int(i)
-    return res
-
-
 """
 N개의 자연수가 입력되면 각 자연수의 자릿수의 합을 구하고, 그 합이 최대인 자연수를 출력
 하는 프로그램을 작성하세요. 각 자연수의 자릿수의 합을 구하는 함수를 def digit_sum(x)를
@@ -12,12 +5,21 @@ N개의 자연수가 입력되면 각 자연수의 자릿수의 합을 구하고
 """
 
 
+def digit_sum(x):
+    res = 0
+    for i in x:
+        res += int(i)
+    return res
+
+
 n = int(input())
 num_list = list(input().split())
-cnt = 0
 max = -1000
-for idx, val in enumerate(num_list):
+ans = 0
+
+for val in num_list:
     if max < digit_sum(val):
         max = digit_sum(val)
-        cnt = idx
-print(num_list[cnt])
+        ans = val
+
+print(ans)
