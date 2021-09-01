d8379a716fc8a7a777afca061ce4e16f4cd9a571
diligejy <sjy049@gmail.com>
2021-09-01 21:31:28 +0900

    Palindrome

--- py_algo_coding/섹션 2/9. 주사위 게임/AA.py
+++ py_algo_coding/섹션 2/9. 주사위 게임/AA.py
@@ -9,8 +9,10 @@ price_list = []
 for num in num_list:
     if num[0] == num[1] == num[2]:
         price_list.append(10000 + num[0] * 1000)
-    elif (num[0] == num[1]) or (num[1] == num[2]) or (num[2] == num[0]):
+    elif (num[0] == num[1]) or (num[2] == num[0]):
         price_list.append(1000 + num[0] * 100)
+    elif num[1] == num[2]:
+        price_list.append(1000 + num[1] * 100)
     else:
         price_list.append(max(num) * 100)
 
---
+++ py_algo_coding/섹션 3/1. 회문 문자열 검사/AA.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+words = [input() for _ in range(n)]
+
+
+def isPalindrome(x):
+    x = x.lower()
+    if x == x[::-1]:
+        return "YES"
+    else:
+        return "NO"
+
+
+for idx, word in enumerate(words):
+    print(f"#{idx + 1} " + isPalindrome(word))
