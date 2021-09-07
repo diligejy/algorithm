8b483f721871009e89d1b4fdbde6fe284cc198d9
diligejy <sjy049@gmail.com>
2021-09-07 21:33:56 +0900

    수들의 합 문제는 어렵다

--- py_algo_coding/섹션 3/5. 수들의 합/AA.py
+++ py_algo_coding/섹션 3/5. 수들의 합/AA.py
@@ -1,4 +1,24 @@
 n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
 
-print(n, m)
-n_list = [i for i in input().split()]
+lt = 0
+rt = 1
+tot = n_list[0]
+cnt = 0
+
+while True:
+    if tot < m:
+        if rt < n:
+            tot += n_list[rt]
+            rt += 1
+        else:
+            break
+    elif tot == m:
+        cnt += 1
+        tot -= n_list[lt]
+        lt += 1
+    else:
+        tot -= n_list[lt]
+        lt += 1
+
+print(cnt)
