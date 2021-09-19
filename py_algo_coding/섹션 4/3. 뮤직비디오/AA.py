71d9949cf6c280682b6a6b7bda2792d25205a4f4
diligejy <sjy049@gmail.com>
2021-09-19 13:04:32 +0900

    add : binary search for music video problem

---
+++ py_algo_coding/섹션 4/3. 뮤직비디오/AA.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+n_list = [int(i) for i in input().split()]
+
+lt = 1
+rt = sum(n_list)
+
+
+def Count(capacity):
+    cnt = 1
+    sum = 0
+    for n in n_list:
+        if sum + n > capacity:
+            cnt += 1
+            sum = n
+        else:
+            sum += n
+    return cnt
+
+
+cnt = 0
+res = 0
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if Count(mid) <= m:
+        res = mid
+        rt = mid - 1
+    else:
+        lt = mid + 1
+print(res)
