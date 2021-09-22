b17831cafba63c55ffab6cb0da8c32b9f9ade2e5
JinyoungSong <ugong2san@bigin.io>
2021-09-22 13:09:30 +0900

    add: increasing sequence

---
+++ py_algo_coding/섹션 4/9. 증가수열 만들기/AA.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+n = int(input())
+n_list = [int(i) for i in input().split()]
+res = ""
+largest = 0
+lt = 0
+rt = n - 1
+tmp = []
+
+while lt <= rt:
+    if n_list[lt] > largest:
+        tmp.append((n_list[lt], "L"))
+    if n_list[rt] > largest:
+        tmp.append((n_list[rt], "R"))
+    tmp.sort()
+    if len(tmp) == 0:
+        break
+    else:
+        res += tmp[0][1]
+        largest = tmp[0][0]
+        if tmp[0][1] == "L":
+            lt += 1
+        else:
+            rt -= 1
+    tmp.clear()
+print(len(res))
+print(res)
