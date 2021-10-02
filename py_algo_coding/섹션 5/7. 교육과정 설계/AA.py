7679d7e32bb590b2673fa06a11074025703e3d84
diligejy <sjy049@gmail.com>
2021-10-02 23:36:44 +0900

    add : design curriculum using deque

--- py_algo_coding/섹션 5/7. 교육과정 설계/AA.py
+++ py_algo_coding/섹션 5/7. 교육과정 설계/AA.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 essential = [i for i in input()]
 n = int(input())
 n_list = [input() for _ in range(n)]
@@ -18,3 +20,20 @@ def isDesignGood(order: int, class_list: str):
 
 for idx, ele in enumerate(n_list):
     isDesignGood(idx + 1, ele)
+
+# queue를 사용한 풀이
+need = input()
+n = int(input())
+for i in range(n):
+    plan = input()
+    dq = deque(need)
+    for x in plan:
+        if x in dq:
+            if x != dq.popleft():
+                print("#%d NO" % (i + 1))
+                break
+    else:
+        if len(dq) == 0:
+            print("#%d YES" % (i + 1))
+        else:
+            print("#%d NO" % (i + 1))
