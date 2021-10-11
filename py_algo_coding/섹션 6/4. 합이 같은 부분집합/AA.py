4dc12c106bfd956d920b5e9f77dde99366cc5b5a
JinyoungSong <ugong2san@bigin.io>
2021-10-11 13:39:42 +0900

    add: subset equal sum

--- py_algo_coding/섹션 6/4. 합이 같은 부분집합/AA.py
+++ py_algo_coding/섹션 6/4. 합이 같은 부분집합/AA.py
@@ -1,21 +1,21 @@
 import sys
-#sys.stdin=open("input.txt", "r")
+
+
 def DFS(L, sum):
-    if sum>total//2:
+    if sum > total // 2:
         return
-    if L==n:
-        if sum==(total-sum):
+    if L == n:
+        if sum == (total - sum):
             print("YES")
             sys.exit(0)
     else:
-        DFS(L+1, sum+a[L])
-        DFS(L+1, sum)
+        DFS(L + 1, sum + n_list[L])
+        DFS(L + 1, sum)
 
-if __name__=="__main__":
-    n=int(input())
-    a=list(map(int, input().split()))
-    total=sum(a)
+
+if __name__ == "__main__":
+    n = int(input())
+    n_list = list(map(int, input().split()))
+    total = sum(n_list)
     DFS(0, 0)
     print("NO")
-
-    
