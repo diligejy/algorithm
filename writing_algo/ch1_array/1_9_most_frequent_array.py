f9a364b6fe2f8b27d8f2d60b2f4f79c1003494fe
diligejy <sjy049@gmail.com>
2021-10-22 22:55:29 +0900

    hashtable

--- writing_algo/ch1_array/1_9_most_frequent_array.py
+++ writing_algo/ch1_array/1_9_most_frequent_array.py
@@ -1,16 +1,32 @@
 from types import List
 
-def majorityElement(nums: List[int]) -> int:
-    """
-    Brute-force
-    """
-    majority_count = int(len(nums) / 2)
+# def majorityElement(nums: List[int]) -> int:
+#     """
+#     Brute-force
+#     """
+#     majority_count = int(len(nums) / 2)
 
-    for i, item_i in enumerate(nums):
-        count = 0 
-        for j, item_j in enumerate(nums[i:], start=i):
-            if item_i == item_j:
-                count += 1
-            if count > majority_count:
-                return item_i
+#     for i, item_i in enumerate(nums):
+#         count = 0
+#         for j, item_j in enumerate(nums[i:], start=i):
+#             if item_i == item_j:
+#                 count += 1
+#             if count > majority_count:
+#                 return item_i
+#     return -1
+
+
+def majorityElement(nums: List[int]) -> int:
+    majority_count = int(len(nums))
+
+    hashmap = {}
+
+    for num in nums:
+        if hashmap.get(num) != None:
+            hashmap[num] = hashmap[num] + 1
+        else:
+            hashmap[num] = 1
+
+        if hashmap[num] > majority_count:
+            return num
     return -1
