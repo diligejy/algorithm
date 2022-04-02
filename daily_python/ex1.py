0eeb6150e8262d0b31f616477bfb2e5a4ec3b1d9
diligejy <sjy049@gmail.com>
2022-04-02 14:29:22 +0900

    add: daily python ex1-2

--- daily_python/ex1.py
+++ daily_python/ex1.py
@@ -1,2 +1,23 @@
+import random
+
+
 def guessing_game():
-    pass
+    real_ans = random.randint(0, 100)
+    while 1:
+        user_ans = input("맞춰보시지~~ 0에서 100사이 정수임 : ")
+        if not user_ans.isdigit():
+            print("제대로 숫자 입력 안할래?")
+            continue
+
+        user_ans = int(user_ans)
+        if user_ans > real_ans:
+            print("Too high")
+        elif user_ans < real_ans:
+            print("Too low")
+
+        if user_ans == real_ans:
+            print(f"Just Right 정답은 {real_ans}였음")
+            return "Just Right"
+
+
+guessing_game()
